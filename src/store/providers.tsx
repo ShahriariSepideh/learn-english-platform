b4464f566fc4e9b71a2d5659a2bf2d98f0759a12
ShahriariSepideh <shahriari.sep@gmail.com>
2026-06-25 17:48:50 +0530

    feat: add auth checker

--- src/store/providers.tsx
+++ src/store/providers.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 import { store } from "./store";
+import { AuthChecker } from "@/components/auth/AuthChecker";
 
 interface ProvidersProps {
     children: ReactNode;
@@ -29,6 +30,7 @@ export function Providers({ children }: ProvidersProps) {
         <ReduxProvider store={store}>
             <QueryClientProvider client={queryClient}>
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                    <AuthChecker />
                     {children}
                     <Toaster richColors position="top-center" />
                 </ThemeProvider>
